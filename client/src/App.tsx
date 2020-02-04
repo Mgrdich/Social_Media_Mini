6237e6e63cce02c158d7d3cbc1691b6199863168
Mgrdich <mgotm13@gmail.com>
2020-02-05 00:11:53 +0400

    Footer issue styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import "./Styles/style.css";
+import Landing from "./components/layout/Landing";
 
 const App = () => {
   return (
     <>
         <div id="container">
             <Header/>
+            <Landing/>
             <Footer/>
         </div>
     </>
