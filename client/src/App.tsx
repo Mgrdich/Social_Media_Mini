c99ed2cb713d077664f2c705831a07986223f177
Mgrdich <mgotm13@gmail.com>
2020-02-04 23:22:42 +0400

    creating Client side Markup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 const App = () => {
   return (
     <div className="App">
-
+        <Header/>
+        <Footer/>
     </div>
   );
 };
