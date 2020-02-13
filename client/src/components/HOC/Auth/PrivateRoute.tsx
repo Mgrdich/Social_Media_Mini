1f068807c9bb54f74611896ae548009c835c7a4c
Mgrdich <mgotm13@gmail.com>
2020-02-13 23:59:23 +0400

    Public and Private Routes

--- client/src/components/HOC/Auth/PrivateRoute.tsx
+++ client/src/components/HOC/Auth/PrivateRoute.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import {Redirect, Route, RouteComponentProps} from "react-router";
 
-interface IPrivateRoute {
-    
+interface IPrivateRoute extends RouteComponentProps {
+    component: any;
+    allowedRoles?: Array<string>;
 }
 
 
 //Route is Accessible only when your Authenticated and have the permissions
-const PrivateRoute = () => {
+const PrivateRoute: React.FC<IPrivateRoute> = ({component: Component, allowedRoles, ...rest}) => {
+    const isAuth = useSelector<any>(state => state.auth.isAuthenticated);
+    const role = useSelector<any>(state => state.auth.user.role);
+
     return (
-        <div>
-            
-        </div>
+        <Route {...rest} render={(props) => (isAuth && allowedRoles && allowedRoles.includes(role as string)) ?
+            (<Component {...rest}/>) :
+            (
+                <Redirect to="/login"/>
+            )}
+        />
     );
 };
 
--- client/src/components/HOC/Auth/PublicRoute.tsx
+++ client/src/components/HOC/Auth/PublicRoute.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import {RouteComponentProps} from "react-router";
 
-interface IPublicRoute {
-
+interface IPublicRoute extends RouteComponentProps{
+    component:React.Component;
+    allowedRoles?: Array<string>;
 }
 
 
 //Route is Accessible only when your not Authenticated Eg -> Login Page
-const PublicRoute = () => {
+const PublicRoute:React.FC<IPublicRoute> = ({component:Component}) => {
     return (
         <div>
             
