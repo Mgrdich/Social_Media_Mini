5da656da4c187b2139590849f13a0ea109c63b3a
Mgrdich <mgotm13@gmail.com>
2020-02-19 23:58:17 +0400

    dashboard types

--- client/src/components/views/dashboard/dashboardTable.tsx
+++ client/src/components/views/dashboard/dashboardTable.tsx
@@ -5,14 +5,14 @@ interface IDashboardTable {
     thead: Array<string>;
     tbody: Array<string>;
     data: Array<any>;
-    style:any;
+    style: any;
 }
 
 const DashboardTable: React.FC<IDashboardTable> = (props) => {
     return (
         <>
             <TableContainer component={Paper} style={{...props.style}} color="secondary">
-                <Table aria-label="simple table">
+                <Table aria-label="simple table" color="primary">
                     <TableHead>
                         <TableRow>
                             {props.thead.map((item: string, index: number) => {
@@ -25,7 +25,7 @@ const DashboardTable: React.FC<IDashboardTable> = (props) => {
                     {props.data.map((row) => (
                         <TableRow key={row._id}>
                             {
-                                props.tbody.map((item: string, index) => {
+                                props.tbody.map((item: string, index: number) => {
                                     return (<TableCell align="center">{row[item]}</TableCell>)
                                 })
                             }
